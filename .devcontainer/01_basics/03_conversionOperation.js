6f92237d56bbb4f26bbfca2fcba319272954914e
Divyanshu Dwivedi <148114353+divyanshu1727@users.noreply.github.com>
2025-05-07 11:31:21 +0000

    commit

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+// let score=33
+// console.log(typeof score)
+
+// let valInNumber=Number(score)
+// console.log(typeof valInNumber)
+
+// let islogedIn=1
+// //let islogedIn=0
+// let boolisloged=Boolean(islogedIn)
+// console.log(boolisloged)
+let num=33
+let strnum=String(num)
+console.log(strnum)
+console.log(typeof strnum)
---
+++ .devcontainer/01_basics/04_operationConversioin.js
@@ -0,0 +1,20 @@
+let val=3
+//console.log(val)
+
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+// console.log(2**2)
+
+// let str1="Hello"
+// let str2=" World"
+// let str3=str1+str2
+// console.log(str3)
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"3")
